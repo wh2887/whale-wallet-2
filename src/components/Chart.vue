8bdb828df53f569f468cc23da8a9dea77b7fc661
McCallWang <13567272790@163.com>
2020-10-08 11:27:29 +0800

    图表显示

---
+++ src/components/Chart.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="echarts-wrapper" ref="wrapper">
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import echarts, {EChartOption, ECharts} from 'echarts';
+  import {Component, Prop} from 'vue-property-decorator';
+
+  @Component
+  export default class Chart extends Vue {
+    @Prop() options?: EChartOption;
+    chart?: ECharts;
+
+
+    mounted() {
+      if (this.options === undefined) {
+        return console.error('options 为空');
+      }
+      this.chart = echarts.init(this.$refs.wrapper as HTMLDivElement);
+      this.chart.setOption(this.options);
+      const div = (this.$refs.wrapper as HTMLDivElement)
+      div.style.width = div.offsetWidth + 'px'
+      div.style.height = div.offsetHeight + 'px'
+
+    }
+
+  }
+</script>
+
+<style lang="scss" scoped>
+  .echarts-wrapper {
+    height: 400px;
+  }
+
+</style>
--- src/views/Statistic.vue
+++ src/views/Statistic.vue
@@ -3,8 +3,8 @@
     <div class="statistic-top">
       <SegmentControls value="-"/>
     </div>
-    <div class="statistic-middle">
-
+    <div class="chart-wrapper" ref="chartWrapper">
+      <Chart class="chart" :options="chartOptions"/>
     </div>
     <div class="statistic-bottom">
 
@@ -16,17 +16,35 @@
   import Vue from 'vue';
   import {Component} from 'vue-property-decorator';
   import SegmentControls from '@/components/SegmentControls.vue';
-  import echarts from 'echarts'
+  import Chart from '@/components/Chart.vue';
 
 
   @Component({
-    components: {SegmentControls}
+    components: {Chart, SegmentControls}
   })
   export default class Statistic extends Vue {
 
-    created(){
-      console.log(echarts);
+    get chartOptions() {
+      return {
+        title: {
+          text: 'ECharts 入门示例'
+        },
+        tooltip: {},
+        legend: {
+          data: ['销量']
+        },
+        xAxis: {
+          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+        },
+        yAxis: {},
+        series: [{
+          name: '销量',
+          type: 'bar',
+          data: [5, 20, 36, 10, 10, 20]
+        }]
+      };
     }
+
 
   }
 </script>
@@ -37,14 +55,20 @@
   .statistic-wrapper {
     @extend %displayAuto;
 
+
+    .echarts {
+      max-width: 100%;
+      height: $chart-height
+    }
+
     .statistic-top {
       width: $width; margin-top: $top-margin/2;
-      background: $color-highlight-thin;  border-radius: $bg-radius;
+      background: $color-highlight-thin; border-radius: $bg-radius;
       padding: .8em 1em;
     }
 
-    .statistic-middle {
-      width: $width; height: 30vh; border-radius: $bg-radius; margin-top: $top-margin/2;
+    .chart-wrapper {
+      width: $width; height: $chart-height; border-radius: $bg-radius; margin-top: $top-margin/2;
       background: $color-f;
     }
 
